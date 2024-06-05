775d40dd983208334537d230e30c3b93c286d648
shayansalehi1381 <shayansalehisut@gmail.com>
2024-06-05 19:31:49 +0330

    Restart Game added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     private bool gameHasEnded = false;
+
+    public Rotator rotator;
+
+    public Spawner spawner;
+
+    public Animator animator;
 
     public void EndGame()
     {
@@ -12,7 +19,25 @@ public class GameManager : MonoBehaviour
         if (gameHasEnded)
             return;
 
+        rotator.enabled = false;
+        spawner.enabled = false;
+
+        animator.SetTrigger("EndGame");
+
+        //make the main camera back ground color to Red
+        Camera.main.backgroundColor = Color.red;
+
         gameHasEnded = true;
-        Debug.Log("End Game");
+     
+    }
+
+
+
+
+    public void RestartGame()
+    {
+        Camera.main.backgroundColor = Color.white;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
     }
 }
